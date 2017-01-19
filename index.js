14895eb51f8ee8f94f49fef5761f5cf705083ea3
hassey13 <ehasselbring@gmail.com>
2017-01-18 19:22:13 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction(fn) {
+  fn()
+}
+
+function returnsANamedFunction() {
+  return receivesAFunction
+}
+
+function returnsAnAnonymousFunction() {
+  return (function(){
+
+  })
+}
